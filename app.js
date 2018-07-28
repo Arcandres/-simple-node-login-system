42da1dba8994f588d10d901c9369ea4cdcd32211
Andres Ardila <Arcandres@hotmail.com>
2018-07-28 18:02:15 -0500

    App created

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 require('./config/config')
 
-const express
+const express = require('express'),
+   mongoose = require('mongoose'),
+   app = express(),
+   port = process.env.PORT;
+
+
+// ROUTES
+app.use(require('./routes/index'))
+
+// DATABASE
+mongoose.connect(process.env.DB, err => err || console.log('DB connection established'))
+
+app.listen(port, () => console.log(`Server running at port: ${port}`));
