d099d9e3963101dc5d2d5d5056ef73ffeb315ab9
Andres Ardila <Arcandres@hotmail.com>
2018-07-28 17:48:53 -0500

    Setting database and enviroment

--- config/config.js
+++ config/config.js
@@ -1,2 +1,8 @@
 // SET PORT
 process.env.PORT = process.env.PORT || 8080;
+
+// SET ENVIROMENT
+process.env.NODE_ENV = process.env.NODE_ENV || true;
+
+// SET DATABASE
+process.env.DB = process.env.NODE_ENV ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI;
