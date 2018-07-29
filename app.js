67081e68ac7d4d077053789b089671e9d6b70a2f
Andres Ardila <Arcandres@hotmail.com>
2018-07-29 13:03:12 -0500

    Adding body-parser

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ require('./config/config')
 
 const express = require('express'),
    mongoose = require('mongoose'),
+   bodyParser = require('body-parser'),
    app = express(),
    port = process.env.PORT;
 
+// PARSE application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+   extended: false
+}))
+
+// PARSE application/json
+app.use(bodyParser.json())
 
 // ROUTES
 app.use(require('./routes/index'))
