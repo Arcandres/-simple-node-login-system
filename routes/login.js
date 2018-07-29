39d4253a2fe57f51fd817e6283654cf7bd0473eb
Andres Ardila <Arcandres@hotmail.com>
2018-07-29 11:31:45 -0500

    User route created

---
+++ routes/login.js
@@ -0,0 +1,8 @@
+const express = require('express'),
+   app = express();
+
+app.get('/login', (req, res) => {
+   // 
+})
+
+module.exports = app;
---
+++ routes/user.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+   app = express();
+
+app.get('/', (req, res) => {
+   // 
+})
+
+app.post('/', (req, res) => {
+   // 
+})
+
+app.put('/', (req, res) => {
+   // 
+})
+
+app.delete('/', (req, res) => {
+   // 
+})
+
+module.exports = app;
