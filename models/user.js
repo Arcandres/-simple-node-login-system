8214d59f42ef1c94d51709845b9bdc1b3041e540
Andres Ardila <Arcandres@hotmail.com>
2018-07-29 12:58:50 -0500

    User model done

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose'),
+   validator = require('mongoose-unique-validator');
+
+const User = mongoose.Schema({
+   name: {
+      type: String,
+      required: true
+   },
+   email: {
+      type: String,
+      unique: true,
+      required: true
+   },
+   password: {
+      type: String,
+      required: true
+   },
+   img: {
+      type: String
+   }
+});
+
+User.methods.toJSON = function () {
+   const obj = this.toObject();
+   delete obj.password;
+
+   return obj;
+}
+
+User.plugin(validator, {message: 'The {PATH} is not available'})
+
+module.exports = mongoose.model('User', User);
