e91cd7b071960c12b07635d032c8fc98a74e3c96
Andres Ardila <Arcandres@hotmail.com>
2018-07-29 20:18:28 -0500

    User CRUD done

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,78 @@
 const express = require('express'),
+   User = require('../models/user'),
    app = express();
 
-app.get('/', (req, res) => {
-   // 
-})
+// Get all users
+app.get('/user', (req, res) => {
+   User
+      .find({})
+      .exec((err, users) => {
+         if (err) {
+            return res.json(err)
+         }
 
-app.post('/', (req, res) => {
-   // 
-})
+         res.json(users)
+      })
+});
 
-app.put('/', (req, res) => {
-   // 
-})
+// Get user by username
+app.get('/user/:username', (req, res) => {
+   User
+      .findOne({username: req.params.username})
+      .exec((err, userDB) => {
+         if (err || !userDB) {
+            return res.json(err)
+         }
 
-app.delete('/', (req, res) => {
-   // 
-})
+         res.json({
+            user: userDB
+         })
+      })
+});
+
+// Create New User
+app.post('/user', (req, res) => {
+   const user = new User({
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+   });
+
+   user.save((err, userDB) => {
+      if (err) {
+         return res.json(err)
+      }
+
+      // Showing user saved in DB
+      res.json({
+         user: userDB
+      })
+   })
+});
+
+// Update user
+app.put('/user/:username', (req, res) => {
+   User.findOneAndUpdate({username: req.params.username}, req.body, {new: true}, (err, userDB) => {
+      if (err) {
+         return res.json(err)
+      }
+
+      res.json({
+         user: userDB
+      })
+   })
+});
+
+// Delete user
+app.delete('/user/:username', (req, res) => {
+   User.findOneAndDelete({username: req.params.username}, (err, userDB) => {
+      if (err) {
+         return res.json(err)
+      }
+
+      res.json(userDB)
+   })
+});
 
 module.exports = app;
