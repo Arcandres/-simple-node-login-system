ffbd1b324c85d9a08936cf537348683f54d8e903
Andres Ardila <Arcandres@hotmail.com>
2018-07-30 00:53:28 -0500

    Login

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,21 @@
 const express = require('express'),
+   User = require('../models/user'),
    app = express();
 
 app.get('/login', (req, res) => {
    res.end('Login...')
 })
+
+app.post('/login', (req, res) => {
+   User.findOne({username: req.body.username}, (err, userDB) => {
+      if (!userDB || req.body.password !== userDB.password) {
+         return res.json('User or password incorrect')
+      }
+
+      res.json(userDB)
+
+   })
+})
+
 
 module.exports = app;
